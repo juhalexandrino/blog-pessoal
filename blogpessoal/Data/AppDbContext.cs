2a24e735a38fb273c37061b93c16f8e27cdca279
juhalexandrino <juliasalexandrino@hotmail.com>
2023-10-09 15:35:43 -0300

    alterando AppDbContext

--- blogpessoal/Data/AppDbContext.cs
+++ blogpessoal/Data/AppDbContext.cs
@@ -41,7 +41,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
@@ -54,7 +54,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = DateTimeOffset.UtcNow;
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
